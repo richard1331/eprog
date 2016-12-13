6a8d2cf47fc1d6352fe29b6b04c849778bf71269
Richard B. Rivera <rivera.richard1331@gmail.com>
2016-12-13 08:59:27 +0800

    Create lab1a.c

---
+++ lab1a.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<math.h>
+#include<conio.h>
+ main()
+{
+
+	int a;
+	int b;
+	int c;
+	int d;
+	int e;
+
+
+
+	printf("enter value of a:\n");
+	scanf("%d", &a);
+	
+	printf("enter value of b:\n");
+	scanf("%d", &b);
+	printf("enter value of c:\n");
+	scanf("%d", &c);
+	printf("enter value of d:\n");
+	scanf("%d", &d);
+	printf("enter value of e:\n");
+	scanf("%d", &e);
+	
+	printf("average equals %d\n", (a+b+c+d+e)/5);
+	
+
+
+
+
+}
