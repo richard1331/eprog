4decb132cd5f9ed6a65def5c84a4558a40aa65c0
Richard B. Rivera <rivera.richard1331@gmail.com>
2016-12-13 08:53:41 +0800

    Create pythagorean.c

---
+++ pythagorean.c
@@ -0,0 +1,26 @@
+
+#include<stdio.h>
+#include<math.h>
+#include<conio.h>
+ main()
+{
+
+	int a;
+	int b;
+	int c;
+
+
+
+	printf("enter value of a:\n");
+	scanf("%d", &a);
+	
+	printf("enter value of b:\n");
+	scanf("%d", &b);
+
+	printf("a square plus b square equals %d\n", (a*a)+(b*b));
+	
+
+	printf("c is equal to %d", c= (sqrt((a*a)+(b*b))));
+
+
+}
