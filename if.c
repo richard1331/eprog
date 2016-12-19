70bd258d2efa9f1198558c53ddfdebba4249a69f
Richard B. Rivera <rivera.richard1331@gmail.com>
2016-12-20 07:43:37 +0800

    Update if.c

--- if.c
+++ if.c
@@ -1,8 +1,14 @@
 #include<stdio.h>
+
 int main(){
 	int grade;
-	
-	printf("Enter grade: ");
-	scanf("%d", &grade);
-	printf("Grade is: %d", grade);	
+  
+	printf("Enter grade:");
+	scanf ("%d", &grade);
+	printf("Grade is: %d\n",grade);
+	if (grade>=75){
+		printf("passed\n");
+	}else{
+		printf("failed\n");
+	}
 }
